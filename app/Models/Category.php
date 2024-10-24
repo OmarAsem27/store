d3a1fa28f53c18f690ac9c31ea421b2948a51f16
OmarAsem27 <omarasem6215@gmail.com>
2024-10-24 14:11:13 +0300

    added a custom validation 'filter'

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Rules\Filter as RulesFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Validation\Rule;
@@ -22,7 +23,20 @@ class Category extends Model
     public static function rules($id = 0)
     {
         return [
-            'name' => "required|string|min:3|max:255|unique:categories,name,$id",
+            'name' => [
+                'required',
+                'string',
+                'min:3',
+                'max:255',
+                'unique:categories,name,$id',
+                /* function ($attributes, $value, $fails) {
+                      if (strtolower($value) == 'laravel') {
+                          $fails("the name can't be $value");
+                      }
+                }*/
+                // new RulesFilter
+                'filter:laravel,php,html'
+            ],
             // Rule::unique('categories', 'name')->ignore($id),
             'parent_id' => ['nullable', 'int', 'exists:categories,id'],
             'image' => ['image', 'max:1048576', 'dimensions:min_width=100,min_height=100'],
