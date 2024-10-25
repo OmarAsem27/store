61e2d904f31dc9e6faf31df802af3aae6b3df5da
OmarAsem27 <omarasem6215@gmail.com>
2024-10-25 15:28:29 +0300

    edited sidebar menu to make the navigation easier

---
+++ config/nav.php
@@ -0,0 +1,31 @@
+<?php
+
+
+return [
+    [
+        'icon' => 'nav-icon fas fa-tachometer-alt',
+        'link' => 'dashboard.dashboard',
+        'title' => 'Dashboard',
+        'active' => 'dashboard.dashboard'
+    ],
+    [
+        'icon' => 'nav-icon far fa-circle',
+        'link' => 'dashboard.categories.index',
+        'title' => 'Categories',
+        'badge' => 'New',
+        'active' => 'dashboard.categories.*'
+    ],
+    [
+        'icon' => 'nav-icon far fa-circle',
+        'link' => 'dashboard.categories.index',
+        'title' => 'Products',
+        'active' => 'dashboard.products.*'
+    ],
+    [
+        'icon' => 'nav-icon far fa-circle',
+        'link' => 'dashboard.categories.index',
+        'title' => 'Orders',
+        'active' => 'dashboard.orders.*'
+    ],
+
+];
